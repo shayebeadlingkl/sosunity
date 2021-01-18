f866e8198f4f722f673699c9e1f1fcdd6a7e6c34
colew-picaro <colevohs@gmail.com>
2021-01-17 23:31:59 -0500

    Remove tilemap to test UI; Add basic UI; Modify character class

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour {
+    // Attach this script to an entity to give it an ability
+    private DamageType damageType;
+    private AbilityType abilityType;
+
+    private void start () {
+
+    }
+
+    // Run this ability on the list of targets
+    public void Run (List<Target> targets) {
+
+    }
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class representing a Character in the game
+// Used in and out of combat
 public class Character : MonoBehaviour
 {
-    private Sprite m_sprite;
+    private Sprite sprite;
+    private float hp;
+    private float attack;
+    private float defense;
+    private float spellcasting; 
+    private float speed;
+    private float spellDefense;
+    private float evasion;
+    private int numActions;
+    private List<Ability> abilities;
+
+    public Sprite getSprite () { return this.sprite; }
+    public float getHp () { return this.hp; }
+    public float getAttack () { return this.attack; }
+    public float getDefense () { return this.defense; }
+    public float getSpellcasting () { return this.spellcasting; }
+    public float getSpeed () { return this.speed; }
+    public float getSpellDefense () { return this.spellDefense; }
+    public float getEvasion () { return this.evasion; }
+    public int getNumActions () { return this.numActions; }
+    public List<Ability> getAbilities () { return this.abilities; }
 
     public void SetSprite (Sprite sprite) {
-        this.m_sprite = sprite;
+        this.sprite = sprite;
+    }
+
+    public void SetTarget () {
+       
     }
 }
