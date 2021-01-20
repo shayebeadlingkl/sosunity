65ceadc2601fc49afd01226dec5e3fe77ab2a849
colew-picaro <colevohs@gmail.com>
2021-01-19 20:26:13 -0500

    Change party class to store GameObjects; Change variable names to reflect what they actually are; Attach character scripts to combatants

--- Assets/Scripts/CombatHandler.cs
+++ Assets/Scripts/CombatHandler.cs
@@ -14,24 +14,37 @@ public class CombatHandler : MonoBehaviour
     
     private Party playerParty;
     private Party enemyParty;
+    private GameObject activeCharacter;
 
     public void Setup(Party playerParty, Party enemyParty) {
         this.playerParty = playerParty;
         this.enemyParty = enemyParty;
+        this.activeCharacter = playerParty.GetParty()[0];
         InitCharacters();
     }
 
     private void Start() {
+
     }
 
     private void InitCharacters () {
-        List<Character> party = playerParty.GetParty();
-        foreach (Character character in party) {
-            //Instantiate(char, new Vector3(0, 0), Quaternion.identity, this.GetComponent<Transform>());
-        }
-        party = enemyParty.GetParty();
-        foreach (Character character in party) {
-        }
+
+    }
+
+    private void OnAttackButton () {
+
+    }
+
+    private void OnDefendButton () {
+
+    }
+
+    private void OnSkillButton () {
+
+    }
+
+    private void OnRunButton () {
+
     }
 
 }
