d820196dbaeb1123031f8fcca0c117413d724207
booperxd <coopervohs@gmail.com>
2021-01-20 01:15:38 -0500

    Basic overworld movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    public float SmoothingFactor;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPos = Target.position + Offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPos, SmoothingFactor *Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerOverworldMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOverworldMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 movement;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        RB.MovePosition(RB.position + movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
